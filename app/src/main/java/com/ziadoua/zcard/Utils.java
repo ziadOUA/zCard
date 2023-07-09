c53ba3577130c13bbe0097bdaa4ad040e0499979
Ziad OUALHADJ <ziadouapro@outlook.com>
2023-07-09 20:52:01 +0200

    fixes #18 - Added a grey theme

--- app/src/main/java/com/ziadoua/zcard/Utils.java
+++ app/src/main/java/com/ziadoua/zcard/Utils.java
@@ -538,6 +538,8 @@ public class Utils {
             theme.applyStyle(R.style.green, true);
         } else if (color.equals(resources.getString(R.string.settings_key_brown_theme))) {
             theme.applyStyle(R.style.brown, true);
+        } else if (color.equals(resources.getString(R.string.settings_key_grey_theme))) {
+            theme.applyStyle(R.style.grey, true);
         } else if (color.equals(resources.getString(R.string.settings_key_catima_theme))) {
             // catima theme is AppTheme itself, no dynamic colors nor applyStyle
         } else {
